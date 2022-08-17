7377abf9fed99f4a45645dab668babf82543bd67
thiago medina <thiagomedina@master.local>
2022-08-17 20:20:33 -0300

    init

---
+++ src/test/java/com/adminvideos/adminvideo/AdminVideoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.adminvideos.adminvideo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdminVideoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
