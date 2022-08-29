2a965905876158d11cd223252e11ee568bc5787b
thiago medina <thiagomedina@master.local>
2022-08-29 20:15:46 -0300

    feat: category class

---
+++ admin_video_catalog/domain/src/test/java/com/admin/video/catalog/domain/category/CategoryTest.java
@@ -0,0 +1,20 @@
+package com.admin.video.catalog.domain.category;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class CategoryTest {
+    @Test
+    public void givenAValidParams_whenCallNewCategory_thenInstantiateACategory() {
+        final var expectedName = "Name";
+        final var expectedDescription = "Description";
+        final var expectedIsActive = true;
+
+        final var actualCategory = Category.newCategory(expectedName, expectedDescription, expectedIsActive);
+        Assertions.assertNotNull(actualCategory);
+        Assertions.assertNotNull(actualCategory.getId());
+        Assertions.assertEquals(expectedName, actualCategory.getName());
+        Assertions.assertEquals(expectedDescription, actualCategory.getDescription());
+        Assertions.assertEquals(expectedIsActive, actualCategory.isActive());
+    }
+}
