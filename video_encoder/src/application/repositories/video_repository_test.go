db3227d0733f3a50de9cd7c92163397184804e5d
Thiago-medina <thiagomedina001@gmail.com>
2022-10-13 19:57:47 -0300

    test:video repo

---
+++ video_encoder/src/application/repositories/video_repository_test.go
@@ -0,0 +1,30 @@
+package repositories_test
+
+import (
+	"encoder/src/application/repositories"
+	"encoder/src/domain"
+	"encoder/src/infra/database"
+	uuid "github.com/satori/go.uuid"
+	"github.com/stretchr/testify/require"
+	"testing"
+	"time"
+)
+
+func TestVideoRepositoryDbInsert(t *testing.T) {
+	db := database.NewDbTest()
+	defer db.Close()
+
+	video := domain.NewVideo()
+	video.ID = uuid.NewV4().String()
+	video.FilePath = "path"
+	video.CreatedAt = time.Now()
+
+	repo := repositories.VideoRepositoryDb{Db: db}
+	repo.Insert(video)
+
+	v, err := repo.Find(video.ID)
+
+	require.NotEmpty(t, v.ID)
+	require.Nil(t, err)
+	require.Equal(t, v.ID, video.ID)
+}
